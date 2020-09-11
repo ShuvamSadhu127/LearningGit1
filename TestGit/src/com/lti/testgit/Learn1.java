fefee53ec17071d3ed0470082d2f4535d2d81fa4
ShrabastiMondal <Shrabasti.Mondal@lntinfotech.com>
2020-09-11 11:32:32 +0530

    login added

--- TestGit/src/com/lti/testgit/Learn1.java
+++ TestGit/src/com/lti/testgit/Learn1.java
@@ -2,9 +2,12 @@ package com.lti.testgit;
 
 public class Learn1 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		 
+		public void login(){
+			System.out.println("logged in...");
+		}
 		
-	}
+	
 
 }
